548961f06ff9f948fc979472dc669f89ffcb785a
Curt Haldorson <haldorsc@oregonstate.edu>
2022-09-02 09:54:02 -0700

    Initialize fragment-implementation

---
+++ app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.honorscollegeapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.honorscollegeapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
